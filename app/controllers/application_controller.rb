bf5d8892ecee65fda63872a083b56294fe7f4d5b
mhamza219 <mhamza219@gmail.com>
2025-01-12 23:29:53 +0530

    changed jwt logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  # include JwtToken
+
+  # skip_before_action :authenticate
+
+  # def authenticate
+  #   header = request.headers['Authorization']
+  #   header = header.split(' ').last if header
+  #   begin
+  #     @decoded = jwt_decode(header)
+  #     @current_user = Resident.find(@decoded[:user_id])
+  #   rescue ActiveRecord::RecordNotFound => e
+  #     render json: {errors: e.message}, status: :unauthorized
+  #   rescue JWT::DecodeError => e
+  #     render json: {errors: e.message}, status: :unauthorized
+  #   end
+  # end
 end
